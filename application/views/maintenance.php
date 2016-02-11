7935ff0e5e0ed9eca17adbb86c09199d6c2882e3
Tech-Imp <techimp.program@gmail.com>
2016-02-11 09:54:11 -0600

    NEW: added a generic down for maintenance html for the use of upgrading to codeigniter. Just need to pull up documentation of 2.x to 3 and swap out the system folder primarily

---
+++ application/views/maintenance.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>Meta-Game Productions</title>
+	<script src="https://meta-game.net/assets/scripts/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://meta-game.net/assets/style/bootstrap.min.css" type="text/css" />
+	<link rel="stylesheet" href="https://meta-game.net/assets/style/bootstrap-theme.min.css" type="text/css" />
+	
+</head>
+<body>
+	
+
+<div class='containter-fluid'>
+	 
+		<div class='row-fluid'>
+			<div  id='welcomeImage' class='col-xs-6 col-xs-offset-3'>
+				<img class='img-responsive' src="../assets/image/MG WIP.png" alt="Website coming soon"  />
+				<br>
+				<h3 class="text-center">Bringing you Reviews, Games, and Awesome</h3>
+				<br>
+				<h4>Site access: Down for maintenance</h4>
+				<!-- <div class="progress">
+			  		<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="97" aria-valuemin="0" aria-valuemax="100" style="width: 96%">
+		    			<span class="sr-only">97% Complete</span>
+		    			97% Complete-Just a little while longer...
+			  		</div>
+				</div> -->	
+				<h2 class="text-center"><s>Official Launch: End of January 2016</s> Soft Launch: February 2016, with some <a href="http://south.paxsite.com/" target="_blank">PAX South goodness</a></h2>
+			</div>
+			<!-- Add an anchor button here later  -->
+		</div>
+</div>
+
+</body>
+	<br>
+	<div class="container-fluid">
+		<div class="row">
+			<div class='col-xs-12 col-md-offset-5 col-md-3 contentFooter'>
+				<strong>Copyright &copy; 2014-<?php echo date("Y"); ?> Meta-Game Productions</strong>
+				<div>Website built by Tech-Imp</div>
+				<div>Built upon the Codeigniter Framework, jQuery, Twitter Bootstrap</div>
+			</div>
+		</div>
+	</div>
+</html>
