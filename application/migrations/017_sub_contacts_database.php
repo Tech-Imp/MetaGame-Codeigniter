5ca82cd977eca9daa20a23589a9e8c5357aa55d0
Tech-Imp <techimp.program@gmail.com>
2016-03-18 13:30:42 -0500

    NEW: Added new table and cleaned up statics table (deleted) to have a unified way of referencing users by subdivision. Users can have access to a subdivision that can have social media, a logo, and a text blurb.
    Subsite database maintains the directory things should fall under and internal info on what the tab should have. sub auth has which users have access to that section for adding new content (there is no subdivision further currently for entities to be entirely autonomous permissions wise) and sub info database contains all the meta information such as social media, logo and forward facing blurb about the subdivision. It also maintains a forSection/exclusive variable to allow these items to branch from subdivisions for specific projects without cluttering things.
    
    Page visibility is still overall governed by page visibility based on generalized permissions.

---
+++ application/migrations/017_sub_contacts_database.php
@@ -0,0 +1,80 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_sub_contacts_database extends CI_Migration {
+
+	//This migration is adding the last piece of the puzzle to allow each 
+	//subdirectory to manage its own social media presence
+	//It also renames the subdirectory table and removes the unused static database
+	//as it was ineffective for this new model
+
+	public function up()
+	{
+		$this->dbforge->add_field(array(
+			'sub_dir'=> array(
+				'type' => 'VARCHAR',
+				'constraint' => 128
+			),
+			'facebook_url'=>array(
+				'type' => 'VARCHAR',
+				'constraint' => 128
+			),
+			'youtube_url'=>array(
+				'type' => 'VARCHAR',
+				'constraint' => 128
+			),
+			'twitter_url'=>array(
+				'type' => 'VARCHAR',
+				'constraint' => 128
+			),
+			'tumblr_url'=>array(
+				'type' => 'VARCHAR',
+				'constraint' => 128
+			),
+			'body'=>array(
+				'type' => 'text',
+				'null' => FALSE,				
+			),
+			'logoID'=>array(
+				'type' => 'INT',
+				'constraint' => 12,
+				'unsigned' => TRUE
+			),
+			'created' => array(
+				'type' => 'DATETIME',
+				'null'=> FALSE,
+			),
+			'modified' => array(
+				'type' => 'DATETIME',
+				'null'=> FALSE,
+			),
+			'author_id' => array(
+				'type' => 'INT',
+				'constraint' => 12,
+				'unsigned' => TRUE,
+			),
+			'forSection' => array(
+				'type' => 'VARCHAR',
+				'constraint' => 128,
+				'null'=> FALSE
+			),
+			'exclusiveSection'=>array(
+				'type' => 'TINYINT',
+				'constraint' => 1,
+				'default' => 0
+			)
+		));
+		
+		$this->dbforge->add_key('sub_dir', true);
+		$this->dbforge->create_table('sub_info_database', TRUE);
+		if ($this->db->table_exists('extra_subsites')){$this->dbforge->rename_table('extra_subsites', 'subsite_database');}
+		$this->dbforge->drop_table('static_database', TRUE);
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_table('sub_info_database', TRUE);
+	}
+
+	
+}
