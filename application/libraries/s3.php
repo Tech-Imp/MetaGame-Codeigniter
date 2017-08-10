1682554753cd0764e22612c3de818904b80347ed
Tech-Imp <techimp.program@gmail.com>
2017-08-10 03:26:31 -0500

    UPDATE: S3 has basic template

---
+++ application/libraries/s3.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+use Aws\S3\S3Client;
+
+//Custom library for accessing AWS for large file storage and retrieval
+
+
+class s3{
+     
+}
